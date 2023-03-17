61d2df609152492cd173c807d22503a61192bf3c
cgwol <cgwalterson@icloud.com>
2023-03-17 03:31:47 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="banner">
+        <div className="banner-left">
+          <h1>Contak</h1>
+        </div>
+        <div className="banner-right">
+          <button className="view-plans">View Plans</button>
+          <div className="dropdown">
+            <button className="login">Login</button>
+            <div className="dropdown-content">
+              <a href="#">Member</a>
+              <a href="#">Creator</a>
+              <a href="#">Artist</a>
+            </div>
+          </div>
+          <button className="signup">Sign Up</button>
+        </div>
+      </div>
+      <div className="App-body">
+        <h2>Getting Started</h2>
+        <div className="user-archetypes">
+          <div className="user-archetype">
+            <h3>Member</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod enim quis mauris varius volutpat. Nulla facilisi. Sed pellentesque, enim eu tristique fermentum, mi odio consequat velit, vitae malesuada sapien velit id diam.</p>
+          </div>
+          <div className="user-archetype">
+            <h3>Creator</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod enim quis mauris varius volutpat. Nulla facilisi. Sed pellentesque, enim eu tristique fermentum, mi odio consequat velit, vitae malesuada sapien velit id diam.</p>
+          </div>
+          <div className="user-archetype">
+            <h3>Artist</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod enim quis mauris varius volutpat. Nulla facilisi. Sed pellentesque, enim eu tristique fermentum, mi odio consequat velit, vitae malesuada sapien velit id diam.</p>
+          </div>
+        </div>
+        <h2>Get Started Today</h2>
+        <p>Placeholder Text</p>
+        <button>Sign Up Now</button>
+      </div>
+      <footer className="App-footer">
+        <p>&copy; 2023 Contak</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
