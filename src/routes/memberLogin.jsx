878281998dc43f5ced3f3b606a4b366d65da378d
Liam O'Dell <liam.riley.odell@gmail.com>
2023-04-17 14:48:56 -0700

    HELP ME

--- src/routes/memberLogin.jsx
+++ src/routes/memberLogin.jsx
@@ -1,16 +1,43 @@
 import Navbar from 'Components/navbar';
 import { useId, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
+import { userCookies } from 'react-cookie';
 
+
+// Sample user database
+const loginInfo = [
+    {username: "musician", password: "1234"},
+    {username: "artist", password: "1234"}
+]
+
+const hoursToSeconds = (hours) => hours * 3600;
 
 export default function MemberLogin() {
     const usernameId = useId(), passwordId = useId();
     const username = useRef(), password = useRef();
     const navigate = useNavigate();
 
+
+    // https://github.com/reactivestack/cookies/tree/master/packages/react-cookie/#getting-started
+    const [cookies, setCookie] = useCookies(['user']); //username cookie, used for login
+
+    const validateLogin = () => {
+        const validUser = loginInfo.find(info => info.username === username.current.value && info.password === password.current.value);
+        const isValidLogin = validUser != null;
+        
+        if (isValidLogin)
+        {
+            setCookie('user', username.current.value, {path: "/", maxAge: hoursToSeconds(1)});
+        }
+
+        return isValidLogin;
+    }
+
     const onLogin = e => {
-        alert(`valid login for '${username.current.value}'`);
-        navigate("/");
+        if (validateLogin()) {
+            alert(`valid login for '${username.current.value}'`);
+            navigate("/");
+        }
     }
 
     return (
